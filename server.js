7f2a25a3cfe7b82aec658700f05754c462e2b731
Meriam Reffass <reffass.meriam@gmail.com>
2022-07-29 12:11:08 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const connectDB =require('./config/db');
+const app = express();
+// Connect Database
+connectDB();
+app.get('/', (req,res)=>res.send('API RUNNING'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`server started on port ${PORT}`));
